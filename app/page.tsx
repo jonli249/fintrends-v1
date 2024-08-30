f9414178253f74b285aa88cfb44539c0c36ec758
Jon Li <jli@Jons-MBP.lan>
2024-08-30 10:19:25 -0700

    Trying to launch deployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import GoogleTrendsSearch from '@/components/trendsform'
 
+//adding a comment randomly
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
